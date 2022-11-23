75de7b10cb4ffd8f7750e74d39c3e065b64ee810
goflutterjava <goflutterjava@gmail.com>
2022-11-23 17:17:48 +0800

    first commit

---
+++ pulsar-rabbitmq-job/src/main/java/io/github/flink/PulsarRabbitmqJob.java
@@ -0,0 +1,8 @@
+package io.github.flink;
+
+public class PulsarRabbitmqJob {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ rabbitmq-pulsar-job/src/main/java/io/github/flink/RabbitmqPulsarJob.java
@@ -0,0 +1,8 @@
+package io.github.flink;
+
+public class RabbitmqPulsarJob {
+
+    public static void main(String[] args) {
+    }
+
+}
