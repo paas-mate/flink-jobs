bbb3329edf15d94cec11fd23bc9f8a9707f06e1a
goflutterjava <goflutterjava@gmail.com>
2022-11-23 17:48:53 +0800

    add job detail

--- pulsar-rabbitmq-job/src/main/java/io/github/flink/PulsarRabbitmqJob.java
+++ pulsar-rabbitmq-job/src/main/java/io/github/flink/PulsarRabbitmqJob.java
@@ -1,8 +1,37 @@
 package io.github.flink;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.pulsar.source.PulsarSource;
+import org.apache.flink.connector.pulsar.source.enumerator.cursor.StartCursor;
+import org.apache.flink.connector.pulsar.source.reader.deserializer.PulsarDeserializationSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.rabbitmq.RMQSink;
+import org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig;
+import org.apache.pulsar.client.api.SubscriptionType;
+
 public class PulsarRabbitmqJob {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        PulsarSource<String> source = PulsarSource.builder()
+                .setServiceUrl("pulsar://localhost:6650")
+                .setAdminUrl("http://localhost:8080")
+                .setStartCursor(StartCursor.earliest())
+                .setTopics("source-topic")
+                .setDeserializationSchema(PulsarDeserializationSchema.flinkSchema(new SimpleStringSchema()))
+                .setSubscriptionName("source-subscription")
+                .setSubscriptionType(SubscriptionType.Exclusive)
+                .build();
+
+        final RMQConnectionConfig connectionConfig = new RMQConnectionConfig.Builder()
+                .setHost("localhost")
+                .setPort(5000)
+                .build();
+        DataStreamSource<String> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Pulsar Source");
+        stream.addSink(new RMQSink<>(connectionConfig, "sink-queue", new SimpleStringSchema()));
+        env.execute("pulsar rabbitmq job start");
     }
 
 }
--- rabbitmq-pulsar-job/src/main/java/io/github/flink/RabbitmqPulsarJob.java
+++ rabbitmq-pulsar-job/src/main/java/io/github/flink/RabbitmqPulsarJob.java
@@ -1,8 +1,36 @@
 package io.github.flink;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.pulsar.sink.PulsarSink;
+import org.apache.flink.connector.pulsar.sink.writer.serializer.PulsarSerializationSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.rabbitmq.RMQSource;
+import org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig;
+
 public class RabbitmqPulsarJob {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        final RMQConnectionConfig connectionConfig = new RMQConnectionConfig.Builder()
+                .setHost("localhost")
+                .setPort(5000)
+                .build();
+        final DataStream<String> stream = env.addSource(new RMQSource<>(
+                        connectionConfig,
+                        "queueName",
+                        true,
+                        new SimpleStringSchema())).setParallelism(1);
+        PulsarSink<String> sink = PulsarSink.builder()
+                .setServiceUrl("pulsar://localhost:6650")
+                .setAdminUrl("http://localhost:8080")
+                .setTopics("sink-topic")
+                .setSerializationSchema(PulsarSerializationSchema.flinkSchema(new SimpleStringSchema()))
+                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
+                .build();
+        stream.sinkTo(sink);
+        env.execute("rabbitmq pulsar job start");
     }
 
 }
